a150fe73073c2c514a4c32124c4cea7e36390ca3
Reza <mrezaurrahman@live.com>
2015-07-30 08:03:13 +0100

    routes file update new

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -26,7 +26,7 @@ Route::get('response', function () {
 });
 
 Route::get('result/{$roll}', function () {
-    return view('greeting', ['name' => 'Masum Hasan bfdsf rewrew etwet fdsf']);
+    return view('greeting', ['name' => 'Masum']);
 });
 
 
